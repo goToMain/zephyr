38bf074960f57fc827d62210d50b3810465f2e34
Siddharth Chandrasekaran <sidcha.dev@gmail.com>
2022-06-07 02:44:01 +0200

    mgmt/osdp: api: Add support for command complete callback
    Add command complete callback so app can subscribe to osdp command or
    event completion events. This can be used to perform post command
    actions such as changing the baud rate of the underlying channel after a
    COMSET command was acknowledged/issued by a peer.
    
    Signed-off-by: Siddharth Chandrasekaran <sidcha.dev@gmail.com>

--- subsys/mgmt/osdp/src/osdp_cp.c
+++ subsys/mgmt/osdp/src/osdp_cp.c
@@ -738,6 +738,9 @@ static int cp_phy_state_update(struct osdp_pd *pd)
 	case OSDP_CP_PHY_STATE_REPLY_WAIT:
 		rc = cp_process_reply(pd);
 		if (rc == OSDP_CP_ERR_NONE) {
+			if (ctx->command_complete_callback) {
+				ctx->command_complete_callback(pd->cmd_id);
+			}
 			pd->phy_state = OSDP_CP_PHY_STATE_CLEANUP;
 			break;
 		}
